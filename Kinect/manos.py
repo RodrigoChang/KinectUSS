e5142a0ffbf6c9f449b834d847f748e27d9e8551
Carlos Tello <102624847+ctellol@users.noreply.github.com>
2023-11-25 23:32:47 -0300

    Update manos.py
    ahora muestran los ángulos en una pantalla aparte, falta pulirlo

--- Kinect/manos.py
+++ Kinect/manos.py
@@ -16,15 +16,22 @@ def calculate_angle(x1, y1, x2, y2, x3, y3):
     angle_deg = math.degrees(angle_rad)
     return angle_deg
 
-# Crear ventana de datos solo con los ángulos
-angle_data = np.zeros((150, 300, 3), dtype=np.uint8)
-angle_data.fill(255)  # Fondo blanco
-cv2.putText(angle_data, f'Ángulos - Izquierda', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-cv2.putText(angle_data, f'Ángulos - Derecha', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-
 # Crear ventanas
 cv2.namedWindow('Hand Tracking', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Angle Data', cv2.WINDOW_NORMAL)
+
+# Crear imagen para mostrar ángulos
+angle_data = np.zeros((300, 600, 3), dtype=np.uint8)
+angle_data.fill(255)  # Fondo blanco
+
+# Crear tablas y textos de nombres de dedos
+cv2.putText(angle_data, 'Ángulos - Izquierda', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+cv2.putText(angle_data, 'Dedo', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+cv2.putText(angle_data, 'Ángulo', (110, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+cv2.putText(angle_data, 'Ángulos - Derecha', (310, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+cv2.putText(angle_data, 'Dedo', (310, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+cv2.putText(angle_data, 'Ángulo', (410, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=2) as hands: 
     while cap.isOpened():
@@ -48,12 +55,18 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, m
         # RGB 2 BGR
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
-        # Detections
-        print(results)
-        
         # Variables para almacenar ángulos
         angle_thumb_left, angle_thumb_right = 0, 0
         angle_left, angle_right = 0, 0
+
+        # Borrar contenido anterior en la ventana de datos
+        angle_data.fill(255)
+        cv2.putText(angle_data, 'Ángulos - Izquierda', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        cv2.putText(angle_data, 'Dedo', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        cv2.putText(angle_data, 'Ángulo', (110, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        cv2.putText(angle_data, 'Ángulos - Derecha', (310, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        cv2.putText(angle_data, 'Dedo', (310, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        cv2.putText(angle_data, 'Ángulo', (410, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
         # Rendering results
         if results.multi_hand_landmarks:
@@ -68,49 +81,42 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, m
                     # Determinar si la mano es izquierda o derecha
                     if hand.landmark[0].x < hand.landmark[9].x:
                         side = 'Izquierda'
-                        angle_text_position = (10, 30)
+                        angle_text_position = (10, 80)
+                        table_start_position = (10, 110)
                     else:
                         side = 'Derecha'
-                        angle_text_position = (10, image.shape[0] - 10)
+                        angle_text_position = (310, 80)
+                        table_start_position = (310, 110)
 
                     # Dedo pulgar
                     if side == 'Izquierda':
-                        angle_thumb_left = calculate_angle(hand.landmark[2].x, hand.landmark[2].y,
+                        angle_thumb_left = calculate_angle(hand.landmark[0].x, hand.landmark[0].y,
                                                            hand.landmark[1].x, hand.landmark[1].y,
-                                                           hand.landmark[0].x, hand.landmark[0].y)
+                                                           hand.landmark[2].x, hand.landmark[2].y)
                     else:
-                        angle_thumb_right = calculate_angle(hand.landmark[2].x, hand.landmark[2].y,
+                        angle_thumb_right = calculate_angle(hand.landmark[0].x, hand.landmark[0].y,
                                                             hand.landmark[1].x, hand.landmark[1].y,
-                                                            hand.landmark[0].x, hand.landmark[0].y)
+                                                            hand.landmark[2].x, hand.landmark[2].y)
 
                     # Otros dedos
-                    for i in range(5, 21):
+                    for i in range(4, 21):
                         if i % 4 == 0:
                             angle = calculate_angle(hand.landmark[i].x, hand.landmark[i].y,
                                                     hand.landmark[i-1].x, hand.landmark[i-1].y,
                                                     hand.landmark[i-2].x, hand.landmark[i-2].y)
                             if side == 'Izquierda':
                                 angle_left = angle
+                                cv2.putText(angle_data, f'Dedo {i//4}', (table_start_position[0], table_start_position[1] + (i//4 - 1) * 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+                                cv2.putText(angle_data, f'{int(angle)}', (table_start_position[0] + 200, table_start_position[1] + (i//4 - 1) * 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
                             else:
                                 angle_right = angle
-                            cv2.putText(image, f'Ángulo {side} Dedo {i//4}: {int(angle)}', (angle_text_position[0], angle_text_position[1] + (i//4) * 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                                cv2.putText(angle_data, f'Dedo {i//4}', (table_start_position[0], table_start_position[1] + (i//4 - 1) * 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+                                cv2.putText(angle_data, f'{int(angle)}', (table_start_position[0] + 200, table_start_position[1] + (i//4 - 1) * 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
             
         # Mostrar resultados en la ventana de tracking
         cv2.imshow('Hand Tracking', image)
 
         # Mostrar ángulos en la ventana de datos
-        cv2.putText(angle_data, f'Ángulo Izq Pulgar: {int(angle_thumb_left)}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Izq Dedo 1: {int(angle_left)}', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Izq Dedo 2: {int(angle_left)}', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Izq Dedo 3: {int(angle_left)}', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Izq Dedo 4: {int(angle_left)}', (10, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-
-        cv2.putText(angle_data, f'Ángulo Der Pulgar: {int(angle_thumb_right)}', (10, 160), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Der Dedo 1: {int(angle_right)}', (10, 180), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Der Dedo 2: {int(angle_right)}', (10, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Der Dedo 3: {int(angle_right)}', (10, 220), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-        cv2.putText(angle_data, f'Ángulo Der Dedo 4: {int(angle_right)}', (10, 240), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-
         cv2.imshow('Angle Data', angle_data)
 
         # Save our image    
