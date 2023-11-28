785b260266596effbd5f49233ece05270cef5254
MrPantuflas <otrom.xddd@gmail.com>
2023-11-28 16:27:02 -0300

    cloud rg utils

--- Kinect/KinectSUS/include/utils.h
+++ Kinect/KinectSUS/include/utils.h
@@ -16,18 +16,20 @@ extern libfreenect2::Freenect2Device::ColorCameraParams ColorCameraParams;
 extern libfreenect2::Freenect2Device::IrCameraParams IrCameraParams;
 extern libfreenect2::Freenect2Device::Config config;
 extern pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+extern pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_rgb;
 extern pcl::visualization::PCLVisualizer::Ptr viewer;
 
+//utils.cpp
 void readIni();
-
 void writeIni();
-
-// get set
 void getParams(libfreenect2::Freenect2Device *dev);
 void setParams(libfreenect2::Freenect2Device *dev);
-
 void send_zmq(cv::Mat& frame, zmq::socket_t&& socket, bool encodeado, std::string tipo);
-
+int confirmacion();
+//depth menu
+void menu();
+//cloud
 void getCloudData(libfreenect2::Registration* registration, libfreenect2::Frame* undistorted_frame);
+void getCloudDataRGB(libfreenect2::Registration* registration, libfreenect2::Frame* undistorted_frame, libfreenect2::Frame* registered_frame);
 void visualizePointCloud();
-//void visualizePointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
+void visualizePointCloudRGB();
