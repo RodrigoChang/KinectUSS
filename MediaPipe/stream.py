a3dd6911f7998ebe058391993bbab1a6e5568a9e
MrPantuflas <otrom.xddd@gmail.com>
2023-11-30 14:45:38 -0300

    super efficient code

--- MediaPipe/stream.py
+++ MediaPipe/stream.py
@@ -10,21 +10,55 @@ DEPTH = True
 REGISTERED = True
 
 IP = "127.0.0.1"
+context = zmq.Context()
 
-def receive_depth_frame(socket):
-    global depth_frame
-    frame_bytes = socket.recv()
-    depth_frame = np.frombuffer(frame_bytes, dtype='float32')
-    depth_frame = depth_frame.reshape((height, width))
-    return depth_frame
+def receive_rgb_frame():
+    socketrgb = context.socket(zmq.SUB)
+    socketrgb.setsockopt(zmq.SUBSCRIBE, b"")
+    socketrgb.connect(f'tcp://{IP}:5555')
+    frame_bytes = socketrgb.recv()
+    rgb_frame = np.frombuffer(frame_bytes, dtype='uint8')
+    rgb_frame = cv2.imdecode(rgb_frame, cv2.IMREAD_COLOR)
+    socketrgb.close()
+    return rgb_frame
 
-def receive_frame(socket):
+def receive_32bit_frame(socket):
     frame_bytes = socket.recv()
-    frame = np.frombuffer(frame_bytes, dtype='uint8')
-    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-    #frame = frame.reshape((height, width, 4))
+    frame = np.frombuffer(frame_bytes, dtype='float32')
+    frame = frame.reshape((height, width))
+    socket.close()
     return frame
 
+def receive_ir_frame():
+    global depth_frame
+    socketir = context.socket(zmq.SUB)
+    socketir.setsockopt(zmq.SUBSCRIBE, b"")
+    socketir.connect(f'tcp://{IP}:5556') 
+    frame_bytes = socketir.recv()
+    ir_frame = np.frombuffer(frame_bytes, dtype='float32')
+    ir_frame = ir_frame.reshape((height, width))
+    socketir.close()
+    return ir_frame
+
+def receive_depth_frame():
+    socketdepth = context.socket(zmq.SUB)
+    socketdepth.setsockopt(zmq.SUBSCRIBE, b"")
+    socketdepth.connect(f'tcp://{IP}:5557') 
+    frame_bytes = socketdepth.recv()
+    depth_frame = np.frombuffer(frame_bytes, dtype='float32')
+    depth_frame = depth_frame.reshape((height, width))
+    socketdepth.close()
+    return depth_frame
+
+def receive_reg_frame():
+    socketreg = context.socket(zmq.SUB)
+    socketreg.setsockopt(zmq.SUBSCRIBE, b"")
+    socketreg.connect(f'tcp://{IP}:5558')
+    frame_bytes = socketreg.recv()
+    reg_frame = np.frombuffer(frame_bytes, dtype='uint8')
+    reg_frame = cv2.imdecode(reg_frame, cv2.IMREAD_COLOR)
+    socketreg.close()
+    return reg_frame
 
 def mouse_callback(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -34,31 +68,15 @@ def mouse_callback(event, x, y, flags, param):
 
 def main():
     global height, width
-
-    context = zmq.Context()
-    socketrgb = context.socket(zmq.SUB)
-    socketir = context.socket(zmq.PULL)
-    socketdepth = context.socket(zmq.PULL)
-    socketreg = context.socket(zmq.SUB)
-
-    socketrgb.connect(f'tcp://{IP}:5555') 
-    socketir.connect(f'tcp://{IP}:5556')
-    socketdepth.connect(f'tcp://{IP}:5557')  
-    socketreg.connect(f'tcp://{IP}:5558') 
-
-    socketrgb.subscribe(b'')
-    #socketir.subscribe(b'')
-    #socketdepth.subscribe(b'')
-    socketreg.subscribe(b'')
-
+    
     cv2.namedWindow("Depth")
     cv2.setMouseCallback("Depth", mouse_callback)
 
     while True:
-        rgb_frame = receive_frame(socketrgb)
-        ir_frame = receive_depth_frame(socketir)
-        depth_frame = receive_depth_frame(socketdepth)
-        reg_frame = receive_frame(socketreg)
+        rgb_frame = receive_rgb_frame()
+        ir_frame = receive_ir_frame()
+        depth_frame = receive_depth_frame()
+        reg_frame = receive_reg_frame()
         cv2.imshow("RGB", rgb_frame)
         cv2.imshow("IR", ir_frame / 4096.0)
         cv2.imshow("Depth", depth_frame / 4096.0)
@@ -66,10 +84,6 @@ def main():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    socketrgb.close()
-    socketir.close()
-    socketdepth.close()
-    socketreg.close()
     context.term()
     cv2.destroyAllWindows()
 
