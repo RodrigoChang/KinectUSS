a65b2314336e44f8fd661e28e5c0f2fe3eeafd05
MrPantuflas <otrom.xddd@gmail.com>
2024-03-18 16:28:58 -0300

    added main

--- KinectIA/KinectMain.py
+++ KinectIA/KinectMain.py
@@ -1,19 +1,39 @@
 #Version 1.2
 #importes necesarios
-import Logica.Body_tracking as BT
-import Logica.Bodyanglecalculator as BTA
-import Logica.Handstracking as HT
-#import utiles.PC_camer_streaming as Video_Stream
-#import utiles.Frame_receptor as Server_Connect
-#Linux
-Input_IP= input("IP: ")
-View=input("View Body or Hands: ")
-if View == "Body":
-    model=input("Model: ")
-    if model == "bt":
-        BT.Bodytracking(Input_IP)
-    if model == "bta":    
-        BTA.Bodyanglecalculator(Input_IP)
-if View == "Hands":
-    HT.Hands(Input_IP)
+#import LogicAI.Body_tracking as BT
+#import LogicAI.Bodyanglecalculator as BTA
+#import LogicAI.Handstracking as HT
+import configparser
+import argparse
 
+def start():
+    parser = argparse.ArgumentParser(description='sUs.')
+    config = configparser.ConfigParser()
+
+    parser.add_argument('--gui', action='store_true', help='Disables the GUI')
+    parser.add_argument('--ip', type=str, help='Set IP')
+    parser.add_argument('--view', type=str, help='Set Viewer')
+    parser.add_argument('--model', type=str, help='Set Model')
+
+    #CONFIG
+    config.read('config.ini')
+    gui = config.get('Settings', 'GUI')
+
+    #ARGS
+    args = parser.parse_args()
+    gui = args.gui
+
+    Input_IP = args.ip if args.ip != None else config.get('Settings', 'IP')
+    View = args.view if args.view != None else config.get('Settings', 'View')
+    model = args.model if args.model != None else config.get('Settings', 'Model')
+
+    if View == "Body":
+        if model == "bt":
+            BT.Bodytracking(Input_IP)
+        if model == "bta":    
+            BTA.Bodyanglecalculator(Input_IP)
+    if View == "Hands":
+        HT.Hands(Input_IP)
+
+if __name__ == "__main__":
+    start()
